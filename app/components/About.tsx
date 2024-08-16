5e8aabed0326139c9cef66c8b36d10633ed8befe
Nikolai Buyao Reyes <nikolai00reyes@outlook.ph>
2024-08-16 14:14:00 +0700

    Added About Component

---
+++ app/components/About.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <>
+            <h2 className="aboutHeading"></h2>
+            <ul className="aboutUl">
+                <li className="aboutLi"></li>
+            </ul>
+        </>
+    )
+}
