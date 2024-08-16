b320620ef1234500782ea99badadd72981310055
Nikolai Buyao Reyes <nikolai00reyes@outlook.ph>
2024-08-16 15:15:11 +0700

    Modified App Page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import About from "./components/About";
 export default function Page() {
     return (
         <>
-            <h1>Portfolio</h1>
+            <h1>Nikolai R. - Portfolio</h1>
             <Profile />
             <About />
         </>
