b316768e578ebb2fe6690567379775b5d16e7ad4
Nikolai Buyao Reyes <nikolai00reyes@outlook.ph>
2024-08-16 14:43:06 +0700

    Modified files

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,11 +1,21 @@
 export default function About() {
     return (
         <>
+            <p className="aboutP">Are you looking to have a website created or edited for your small business?</p>
+            <p className="aboutP">I'm here to assist! With 6 years of experience transitioning from quality testing to front-end development, I bring a unique perspective to streamline your workflows and enhance your web applications.</p>
             <h2 className="aboutHeading">What I bring to the table:</h2>
             <ul className="aboutUl">
                 <li className="aboutLi">Expertise in technology for front-end development</li>
                 <li className="aboutLi">Selecting and applying concepts tailored for front-end applications</li>
             </ul>
+            <h2 className="aboutHeading">Technical Skills:</h2>
+            <ul className="aboutUl">
+                <li className="aboutLi">Programming Languages: JavaScript (including frameworks like React)</li>
+                <li className="aboutLi">Website Platform: WordPress</li>
+                <li className="aboutLi">Version Control: Git</li>
+            </ul>
+            <p className="aboutP">I understand it's a bit technical, but let's discuss your specific needs. Together, we can achieve your dream website. Looking forward to chatting soon!</p>
+            <p className="aboutP">Best regards,<br />Nik</p>
         </>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Profile from "./components/Profile";
+import About from "./components/About";
 
 export default function Page() {
     return (
         <>
             <h1>Portfolio</h1>
             <Profile />
+            <About />
         </>
     );
 }
