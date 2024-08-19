6469ceece755369d78696a224d51c6d458408770
Nikolai Buyao Reyes <nikolai00reyes@outlook.ph>
2024-08-19 15:33:00 +0700

    Modified App Page, added files

---
+++ app/api/route.ts
@@ -0,0 +1,13 @@
+import { promises } from "fs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    try {
+        const fileName = request.nextUrl.searchParams.get("fileName");
+        const read = await promises.readFile(process.cwd() + `/app/api/files/${fileName}.json`, "utf8");
+        const parsedRead = JSON.parse(read);
+        return NextResponse.json(parsedRead, { status: 200 });
+    } catch (e) {
+        return NextResponse.json({ eMessage: e.message }, { status: 400 });
+    }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,38 @@
+"use client"
+
+import { useState, useEffect } from "react";
+
 import Header from "./components/Header";
 import Profile from "./components/Profile";
 import About from "./components/About";
 
 export default function Page() {
-    const profileSrc = "./about.svg";
-    const profileAlt = "Nikolai Reyes";
-    const profileHeading = "Wordpress Developer | Front-end Developer | Quality Tester";
+    const [profileSrc, setProfileSrc] = useState("");
+    const [profileAlt, setProfileAlt] = useState("");
+    const [profileH2, setProfileH2] = useState("");
+    const [aboutTableBringings, setAboutTableBringings] = useState("");
+    const [aboutTechnicalSkills, setAboutTechnicalSkills] = useState("");
 
-    const aboutTableBringings = [
-        "Expertise in technology for front-end development",
-        "Selecting and applying concepts tailored for front-end applications"
-    ]
+    useEffect(() => {
+        fetch("/api?fileName=profile", {
+            method: "GET"
+        })
+            .then(profileFile => profileFile.json())
+            .then(jsonProfileFile => {
+                setProfileSrc(jsonProfileFile.src);
+                setProfileAlt(jsonProfileFile.alt);
+                setProfileH2(jsonProfileFile.h2);
+            });
 
-    const aboutTechnicalSkills = {
-        programmingLanguage: "JavaScript",
-        websitePlatform: "WordPress",
-        versionControl: "Git"
-    }
+        fetch("/api?fileName=about", {
+            method: "GET"
+        })
+            .then(aboutFile => aboutFile.json())
+            .then(jsonAboutFile => {
+                setAboutTableBringings(jsonAboutFile.tableBringings);
+                setAboutTechnicalSkills(jsonAboutFile.technicalSkills);
+            });
+    }, []);
 
     return (
         <>
@@ -24,7 +40,7 @@ export default function Page() {
             <Profile
                 src={profileSrc}
                 alt={profileAlt}
-                h2={profileHeading}
+                h2={profileH2}
             />
             <About
                 tableBringings={aboutTableBringings}
