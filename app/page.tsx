e427562262c60709e3ff82017590068198427620
Nikolai Buyao Reyes <nikolai00reyes@outlook.ph>
2024-11-04 09:20:09 +0800

    Modified Page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import homeDatumFile from "./datumFiles/home.json";
 import aboutMeDatumFile from "./datumFiles/aboutMe.json";
 import whatIOfferDatumFile from "./datumFiles/whatIOffer.json";
 import myPortfolioDatumFile from "./datumFiles/myPortfolio.json";
-import myExperiencesDatumFile from "./datumFiles/myExperiences.json";
 import letsConnectDatumFile from "./datumFiles/letsConnect.json";
 import bottomDatumFile from "./datumFiles/bottom.json";
 
@@ -14,7 +13,6 @@ import HomeComponent from "./components/Home";
 import AboutMeComponent from "./components/AboutMe";
 import WhatIOfferComponent from "./components/WhatIOffer";
 import MyPortfolioComponent from "./components/MyPortfolio";
-import MyExperiencesComponent from "./components/MyExperiences";
 import LetsConnectComponent from "./components/LetsConnect";
 import BottomComponent from "./components/Bottom";
 
@@ -23,7 +21,6 @@ import { HomeContext } from "./contexts/Home";
 import { AboutMeContext } from "./contexts/AboutMe";
 import { WhatIOfferContext } from "./contexts/WhatIOffer";
 import { MyPortfolioContext } from "./contexts/MyPortfolio";
-import { MyExperiencesContext } from "./contexts/MyExperiences";
 import { LetsConnectContext } from "./contexts/LetsConnect";
 import { BottomContext } from "./contexts/Bottom";
 
@@ -32,7 +29,6 @@ import HomeInterface from "./interfaces/Home";
 import AboutMeInterface from "./interfaces/AboutMe";
 import WhatIOfferInterface from "./interfaces/WhatIOffer";
 import MyPortfolioInterface from "./interfaces/MyPortfolio";
-import MyExperiencesInterface from "./interfaces/MyExperiences";
 import LetsConnectInterface from "./interfaces/LetsConnect";
 import BottomInterface from "./interfaces/Bottom";
 
@@ -42,7 +38,6 @@ export default function Page(): React.ReactElement {
     const aboutMe: AboutMeInterface = aboutMeDatumFile;
     const whatIOffer: WhatIOfferInterface = whatIOfferDatumFile;
     const myPortfolio: MyPortfolioInterface = myPortfolioDatumFile;
-    const myExperiences: MyExperiencesInterface = myExperiencesDatumFile;
     const letsConnect: LetsConnectInterface = letsConnectDatumFile;
     const bottom: BottomInterface = bottomDatumFile;
 
